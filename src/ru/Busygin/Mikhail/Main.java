d9edfddf62815bdec8ef459756d7f0fb16006338
Mikhail Busygin <mix_ice@mail.ru>
2022-01-25 07:30:45 +0400

    commit

---
+++ src/ru/Busygin/Mikhail/Main.java
@@ -0,0 +1,10 @@
+package ru.Busygin.Mikhail;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
